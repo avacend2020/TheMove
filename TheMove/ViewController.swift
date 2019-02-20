886c3a230ed901f0d9fed399c76ccb4bbae114e0
User 2 <user2@192.168.68.113>
2019-02-20 09:52:57 +0530

    Initial Commit

---
+++ TheMove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMove
+//
+//  Created by User 2 on 2/20/19.
+//  Copyright © 2019 User 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
